dc0c012d07c6be2af7e1a048526ddfdd8760cc5b
Maxnightik <98279735+Maxnightik@users.noreply.github.com>
2023-02-05 19:17:21 +0200

    new

---
+++ js/main.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel();
+  });
+
+
+
+  const owl = $('.owl-carousel');
+owl.owlCarousel( {
+  center: true,
+  loop: true,
+  margin: 30,
+  startPosition: 1,
+  items: 3,
+});
+
+$('.slider__btn--prev').click(function() {
+  // With optional speed parameter
+  // Parameters has to be in square bracket '[]'
+  owl.trigger('prev.owl.carousel');
+})
+
+$('.slider__btn--next').click(function() {
+    owl.trigger('next.owl.carousel');
+})
+
